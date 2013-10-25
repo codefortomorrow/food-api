8822b5638032ef724e08e430055ec08f70b80f5c
Mike Cheng - nsmalab <xuehung@gmail.com>
2013-10-25 11:51:18 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodApi::Application.config.secret_key_base = '2f03a3b19662f73a54731717982334ddc58c773abedfdfe1e45dedcd99638384c89054ad42c2826400b21ceade7305b612ee312440c86241820c7dccc275c975'
