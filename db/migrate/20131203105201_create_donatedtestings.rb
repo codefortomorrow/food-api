5d7dba7679a9445e60d9a8be7b717fa8a83e4646
Mike Cheng <xuehung@gmail.com>
2013-12-26 09:55:24 +0800

    added attributes

--- db/migrate/20131203105201_create_donatedtestings.rb
+++ db/migrate/20131203105201_create_donatedtestings.rb
@@ -3,9 +3,13 @@ class CreateDonatedtestings < ActiveRecord::Migration
     create_table :donatedtestings do |t|
       t.string :proposer
       t.string :title
+      t.string :testers
+      t.string :test_items
+      t.integer:food_id
       t.text   :description
       t.float  :target_amount
       t.float  :current_amount
+      t.integer:current_donators
       t.timestamp :start_date
       t.timestamp :deadline
       
